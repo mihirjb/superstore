bb8aa4803322050674255eb4610087ec3f19b428
Mihir <mihirjb@gmail.com>
2014-02-09 17:51:20 +0800

    removed paperclip valiadtions

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -40,7 +40,6 @@ class Profile < ActiveRecord::Base
   
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>", :mini => "30x30>" }, :default_url => "/images/profiles/avatars/:style/missing.png"
   
-  validates_attachment_content_type :file_name, :content_type => %w(image/jpeg image/jpg image/png)
   
   
 end
