576be92a9a448215f0ddaf09dbd01db03cf6e633
Mihir <mihirjb@gmail.com>
2014-02-25 17:18:24 +0800

    made the referer + profile setter working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,30 +4,26 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   set_current_tenant_by_subdomain(:account, :subdomain)
   after_filter :store_location
-
+  
   def store_location
-    # store last url - this is needed for post-login redirect to whatever the user last visited.
-    if (request.fullpath != "/vendors/sign_in" &&
-        request.fullpath != "/vendors/sign_up" &&
-        request.fullpath != "/vendors/password" &&
-        request.fullpath != "/vendors/sign_out" &&
-        request.fullpath != "/admins/sign_in" &&
-        request.fullpath != "/admins/sign_up" &&
-        request.fullpath != "/admins/password" &&
-        request.fullpath != "/admins/sign_out" &&
-        request.fullpath != "/accounts" &&
-        request.fullpath != "/accounts/new" &&
-        request.fullpath != "/accounts/create" &&
-        request.fullpath != "/accounts/edit" &&
-        !request.xhr?) # don't store ajax calls
-      session[:previous_url] = request.fullpath 
-    end
-  end
-
-  def after_sign_in_path_for(vendor)
-     
-    session[:previous_url] || root_path
-  end 
+     # store last url - this is needed for post-login redirect to whatever the user last visited.
+     if (request.fullpath != "/vendors/sign_in" &&
+         request.fullpath != "/vendors/sign_up" &&
+         request.fullpath != "/vendors/password" &&
+         request.fullpath != "/vendors/sign_out" &&
+         request.fullpath != "/admins/sign_in" &&
+         request.fullpath != "/admins/sign_up" &&
+         request.fullpath != "/admins/password" &&
+         request.fullpath != "/admins/sign_out" &&
+         request.fullpath != "/accounts" &&
+         request.fullpath != "/accounts/new" &&
+         request.fullpath != "/accounts/create" &&
+         request.fullpath != "/accounts/edit" &&
+         !request.xhr?) # don't store ajax calls
+       session[:previous_url] = request.fullpath 
+     end
+   end
+  
  
   def not_found
     raise ActionController::RoutingError.new('Not Found')
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,28 +2,26 @@ class SessionsController < Devise::SessionsController
   
   before_filter :authenticate_vendor!
   
-  def stored_location_for(vendor)
-      nil
-    end
+
  
   def after_sign_in_path_for(vendor)
-    account_id = current_vendor.account_id
-    @account = Account.find(account_id)
-    subdomain = @account.subdomain
+     
+
+      if current_vendor.sign_in_count < 2
+        account_id = current_vendor.account_id
+          @account = Account.find(account_id)
+          subdomain = @account.subdomain
+
+          @profile = Profile.find_by_vendor_id(current_vendor.id)
+        @profile.update_column("displayname",@account.subdomain)
+      end
+     
     
-    @profile = Profile.find_by_vendor_id(current_vendor.id)
-    
-    if @profile.displayname.nil?
-      @profile.update_column("displayname",@account.subdomain)
-    end
-    sign_in_url = url_for(:action => 'new', :controller => 'sessions', :only_path => false, :protocol => 'http')  
-      if (request.referer == sign_in_url)
-        super
-      else
-        request.referer
-       end
-     end
+    session[:previous_url] || root_path
+  end
   
+  
+ 
   def after_sign_out_path_for(vendor)
    root_url()  
   end
