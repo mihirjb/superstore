51ede0c41c0f970aa25c3122b823bbd459fd7b26
Mihir Buch <mihirjb@gmail.com>
2014-11-17 17:18:28 +0800

    added desc to new listed phones

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,7 @@ class PagesController < ApplicationController
   
   def home
          @newlistedphones = Phone.find(:all, :order => "updated_at", :limit => 6)
-         @mostviewedphones = Phone.find(:all, :order => "impressions_count", :limit => 6)
+         @mostviewedphones = Phone.find(:all, :order => "impressions_count DESC", :limit => 6)
          
   end
   
