653e79b3a4ab12a87ddf93b0146bf0dd7453ad99
Mihir Buch <mihirjb@gmail.com>
2014-12-11 16:38:17 +0800

    ipn notifications

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Storeapp::Application.routes.draw do
   post "transactions/processtransaction"
   get "transactions/completetransaction"
   get "transactions/failedtransaction"
-  post "transactions/notify_action"  => 'transactions#notify_action'
+  match 'transactions/notify_action'  => 'transactions#notify_action'
   
   get "phones/index"
   get "phones/new"
