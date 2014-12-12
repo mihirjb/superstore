776b3a711377252d69bfed934e7f1b44e2b7237e
Mihir Buch <mihirjb@gmail.com>
2014-12-12 20:22:56 +0800

    added new way to acces params

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -149,7 +149,7 @@ class TransactionsController < ApplicationController
        when "VERIFIED"
          
          logger.info "It is verified"
-           logger.info "Transaction id for sender is #{params['transaction[0].id_for_sender_txn']}"
+           logger.info "Transaction id for sender is #{response.params['transaction[0].id_for_sender_txn']}"
            
         end
          # check that paymentStatus=Completed
