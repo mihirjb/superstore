a745cfd45beb20c1878e874374b02add1f226bd5
Mihir Buch <mihirjb@gmail.com>
2015-01-15 17:31:01 +0800

    fixed buyer mailer

--- app/mailers/buyer_mailer.rb
+++ app/mailers/buyer_mailer.rb
@@ -4,17 +4,18 @@ class BuyerMailer < ActionMailer::Base
 
   def order_confirmation(buyer,seller, order)
     @order = order
-     @buyer = Profile.find_by_user_id(buyer.id)
-     @seller = Profile.find_by_user_id(seller.user_id)
-      mail(:to => "#{buyer.email}", :subject => "Recipt for your order from Phonesalad")
+     @buyer = Profile.where('user_id =?',buyer.id).first
+     @seller = Profile.where('user_id =?',seller.user_id).first
+     @listing = seller
+      mail(:to => "#{buyer.email}", :subject => "Recipt for your order from Zalpe")
   end
   
   def shipping_confirmation(order,trackcode)
     @order = order
      @trackcode = trackcode
-      @buyerprofile = Profile.find_by_user_id(order.user_id)
-      @buyer = User.find(order.user_id)
-       mail(:to => "#{@buyer.email}", :subject => "Recipt for your order from Phonesalad")
+      @buyerprofile = Profile.find_by_vendor_id(order.vendor_id)
+      @buyer = Vendor.find(order.vendor_id)
+       mail(:to => "#{@buyer.email}", :subject => "Recipt for your order from Zalpe")
    end
   
 end
